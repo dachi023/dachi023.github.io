657955e6901bd231299e59bc030f8118022aa0c2
dachi <ar.19920223@gmail.com>
2019-12-10 01:42:17 +0900

    Add global menu

--- src/organisms/Header.tsx
+++ src/organisms/Header.tsx
@@ -2,19 +2,56 @@ import React from 'react'
 import styled from 'styled-components'
 import Link from 'gatsby-link'
 
-const TitleLink = styled(Link)`
-  color: #130f40;
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+`
+
+const Title = styled.h1`
   font-family: 'Pacifico', cursive;
   font-size: 2.2rem;
+`
+
+const TitleLink = styled(Link)`
+  color: #130f40;
+  text-decoration: none;
+`
+
+const Menu = styled.ul`
+  display: flex;
+`
+
+const MenuItem = styled.li`
+  margin-left: 0.9em;
+  &:first-child {
+    margin: 0;
+  }
+`
+
+const MenuLink = styled(Link)`
+  color: #130f40;
   text-decoration: none;
 `
 
 export default function Header() {
   return (
     <header>
-      <h1>
-        <TitleLink to="/">dachi.work</TitleLink>
-      </h1>
+      <Container>
+        <Title>
+          <TitleLink to="/">dachi.work</TitleLink>
+        </Title>
+        <nav>
+          <Menu>
+            <MenuItem>
+              <MenuLink to="/">About</MenuLink>
+            </MenuItem>
+            <MenuItem>
+              <MenuLink to="/posts">Posts</MenuLink>
+            </MenuItem>
+          </Menu>
+        </nav>
+      </Container>
     </header>
   )
 }
